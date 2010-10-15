9d36f1509336fb63673b0ea00e56d78522a1a256
mat sadler <mat@redstudio.co.uk>
2010-10-15 11:48:43 +0100

    first commit

---
+++ system/application/views/global/footer.php
@@ -0,0 +1,2 @@
+
+Website Developed by <a href="http://www.redstudio.co.uk">Redstudio Design Limited</a>. This site is best viewed in <a href="http://www.mozilla-europe.org/en/firefox/">Mozilla Firefox</a> or <a href="http://www.google.com/chrome">Google Chrome</a>. | 
---
+++ system/application/views/sidebar/profile.php
@@ -0,0 +1,33 @@
+<?php foreach($professional as $profile):?>
+
+<img src="<?=base_url()?>images/profiles/<?=$profile['image_location'];?>">
+<br/>
+
+<div style="float:left;">
+<a target="_blank" href="<?=base_url()?>images/vcards/<?=$profile['vcard'];?>"><img src="<?=base_url()?>images/vcards/vcard.png"></a>
+</div>
+		
+<div style="float:left; padding:8px 0 0 4px;">
+		<strong><?=$profile['firstname'];?> <?=$profile['middlename'];?> <?=$profile['lastname'];?></strong><br/>
+<?=$profile['title'];?>
+
+</div>
+		
+<div style="clear:both;"></div>
+<br/>
+Email: <a href="mailto:<?=$profile['email'];?>"><?=$profile['email'];?></a><br/>
+
+<?php endforeach; ?>
+<br/>
+<hr>
+<h3>Selected Cases</h3>
+<table>
+<?php foreach($cases as $case):?>
+
+<tr style="padding-bottom:3px;">
+<td><img src="<?=base_url()?>images/pdf.png" width="28px"></td>
+<td style="border-bottom:1px #aabbc8 solid; "><a href="<?=base_url()?>uploads/<?=$case['case_file'];?>"><?=$case['case_title'];?></a></td>
+</tr>
+
+<?php endforeach;?>
+</table>
